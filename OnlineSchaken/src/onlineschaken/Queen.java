951de1e6c2d1cacab843a77cf80adcda3b9259b5
Jeroen van Baast <jeroenvanbaast@gmail.com>
2016-10-11 12:08:43 +0200

    checkMove konigin
    Konigin kan nu bewegen

--- OnlineSchaken/src/onlineschaken/Queen.java
+++ OnlineSchaken/src/onlineschaken/Queen.java
@@ -11,24 +11,133 @@ import javafx.scene.image.Image;
  *
  * @author redxice
  */
-public class Queen extends Piece{
-    
-    public Queen(String p_color, Player p_player, Section p_section) {
+public class Queen extends Piece
+{
+
+    public Queen(String p_color, Player p_player, Section p_section)
+    {
         super(p_color, p_player, p_section);
-        if(p_color == "white")
+        if (p_color == "white")
         {
-        this.img = new Image("ChessPieces/White Queen.png");
+            this.img = new Image("ChessPieces/White Queen.png");
         }
-        if(p_color == "black")
+        if (p_color == "black")
         {
-        this.img = new Image("ChessPieces/Black Queen.png");
+            this.img = new Image("ChessPieces/Black Queen.png");
         }
-        
+
     }
 
     @Override
-    public Boolean checkMove(Section p_section) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Boolean checkMove(Section p_section)
+    {
+        if (isValidMove(p_section) == true)
+        {
+            if (p_section.getID().x - this.section.getID().x == p_section.getID().y - this.section.getID().y)
+            {
+                if (this.section.getID().x > p_section.getID().x)
+                {
+                    if (this.section.getID().y > p_section.getID().y)
+                    {
+                        for (int i = 0; i < this.section.getID().y - p_section.getID().y; i++)
+                        {
+                            if (this.section.getBoard().getSections()[this.section.getID().x + i][this.section.getID().y + i] != null)
+                            {
+                                return false;
+                            }
+                        }
+                        return true;
+                    }
+                    if (this.section.getID().y < p_section.getID().y)
+                    {
+                        for (int i = 0; i < this.section.getID().y - p_section.getID().y; i++)
+                        {
+                            if (this.section.getBoard().getSections()[this.section.getID().x + i][this.section.getID().y - i] != null)
+                            {
+                                return false;
+                            }
+                        }
+                        return true;
+                    }
+                } else if (this.section.getID().x < p_section.getID().x)
+                {
+                    if (this.section.getID().y > p_section.getID().y)
+                    {
+                        for (int i = 0; i < this.section.getID().y - p_section.getID().y; i++)
+                        {
+                            if (this.section.getBoard().getSections()[this.section.getID().x - i][this.section.getID().y + i] != null)
+                            {
+                                return false;
+                            }
+                        }
+                        return true;
+                    }
+                    if (this.section.getID().y < p_section.getID().y)
+                    {
+                        for (int i = 0; i < this.section.getID().y - p_section.getID().y; i++)
+                        {
+                            if (this.section.getBoard().getSections()[this.section.getID().x - i][this.section.getID().y - i] != null)
+                            {
+                                return false;
+                            }
+                        }
+                        return true;
+                    }
+                }
+            } else if (p_section.getID().x - this.section.getID().x == p_section.getID().y - this.section.getID().y)
+            {
+                if (this.section.getID().x > p_section.getID().x)
+                {
+                    if (this.section.getID().y > p_section.getID().y)
+                    {
+                        for (int i = 0; i < this.section.getID().y - p_section.getID().y; i++)
+                        {
+                            if (this.section.getBoard().getSections()[this.section.getID().x + i][this.section.getID().y + i] != null)
+                            {
+                                return false;
+                            }
+                        }
+                        return true;
+                    }
+                    if (this.section.getID().y < p_section.getID().y)
+                    {
+                        for (int i = 0; i < this.section.getID().y - p_section.getID().y; i++)
+                        {
+                            if (this.section.getBoard().getSections()[this.section.getID().x + i][this.section.getID().y - i] != null)
+                            {
+                                return false;
+                            }
+                        }
+                        return true;
+                    }
+                } else if (this.section.getID().x < p_section.getID().x)
+                {
+                    if (this.section.getID().y > p_section.getID().y)
+                    {
+                        for (int i = 0; i < this.section.getID().y - p_section.getID().y; i++)
+                        {
+                            if (this.section.getBoard().getSections()[this.section.getID().x - i][this.section.getID().y + i] != null)
+                            {
+                                return false;
+                            }
+                        }
+                        return true;
+                    }
+                    if (this.section.getID().y < p_section.getID().y)
+                    {
+                        for (int i = 0; i < this.section.getID().y - p_section.getID().y; i++)
+                        {
+                            if (this.section.getBoard().getSections()[this.section.getID().x - i][this.section.getID().y - i] != null)
+                            {
+                                return false;
+                            }
+                        }
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
-    
+
 }
