e21514aa4f3dd6d6ba0d1bb69ccf07832337eb95
Jeroen van Baast <jeroenvanbaast@gmail.com>
2016-10-11 12:04:54 +0200

    checkMove
    Loper kan nu bewegen zoals het hoort

--- OnlineSchaken/src/onlineschaken/Bishop.java
+++ OnlineSchaken/src/onlineschaken/Bishop.java
@@ -11,25 +11,82 @@ import javafx.scene.image.Image;
  *
  * @author redxice
  */
-public class Bishop extends Piece{
-    
-    public Bishop(String p_color, Player p_player, Section p_section) {
+public class Bishop extends Piece
+{
+
+    public Bishop(String p_color, Player p_player, Section p_section)
+    {
         super(p_color, p_player, p_section);
-        if(p_color == "white")
+        if (p_color == "white")
         {
-        this.img = new Image("ChessPieces/White Bishop.png");
-        }   
-        if(p_color == "black")
-        {
-        this.img = new Image("ChessPieces/Black Bishop.png");
+            this.img = new Image("ChessPieces/White Bishop.png");
         }
-         
+        if (p_color == "black")
+        {
+            this.img = new Image("ChessPieces/Black Bishop.png");
+        }
+
     }
 
     @Override
-    public Boolean checkMove(Section p_section) {
-       
+    public Boolean checkMove(Section p_section)
+    {
+        if (isValidMove(p_section) == true)
+        {
+            if (p_section.getID().x - this.section.getID().x == p_section.getID().y - this.section.getID().y)
+            {
+                if (this.section.getID().x > p_section.getID().x)
+                {
+                    if (this.section.getID().y > p_section.getID().y)
+                    {
+                        for (int i = 0; i < this.section.getID().y - p_section.getID().y; i++)
+                        {
+                            if (this.section.getBoard().getSections()[this.section.getID().x + i][this.section.getID().y + i] != null)
+                            {
+                                return false;
+                            }
+                        }
+                        return true;
+                    }
+                    if (this.section.getID().y < p_section.getID().y)
+                    {
+                        for (int i = 0; i < this.section.getID().y - p_section.getID().y; i++)
+                        {
+                            if (this.section.getBoard().getSections()[this.section.getID().x + i][this.section.getID().y - i] != null)
+                            {
+                                return false;
+                            }
+                        }
+                        return true;
+                    }
+                } else if (this.section.getID().x < p_section.getID().x)
+                {
+                    if (this.section.getID().y > p_section.getID().y)
+                    {
+                        for (int i = 0; i < this.section.getID().y - p_section.getID().y; i++)
+                        {
+                            if (this.section.getBoard().getSections()[this.section.getID().x - i][this.section.getID().y + i] != null)
+                            {
+                                return false;
+                            }
+                        }
+                        return true;
+                    }
+                    if (this.section.getID().y < p_section.getID().y)
+                    {
+                        for (int i = 0; i < this.section.getID().y - p_section.getID().y; i++)
+                        {
+                            if (this.section.getBoard().getSections()[this.section.getID().x - i][this.section.getID().y - i] != null)
+                            {
+                                return false;
+                            }
+                        }
+                        return true;
+                    }
+                }
+            }
+        }
         return false;
     }
-    
+
 }
