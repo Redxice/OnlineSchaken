6f08661ffc8331118dd9d2aa67144d730794cb04
Jeroen van Baast <jeroenvanbaast@gmail.com>
2016-10-17 12:53:23 +0200

    Chatline gemaakt

--- OnlineSchaken/src/onlineschaken/Chatline.java
+++ OnlineSchaken/src/onlineschaken/Chatline.java
@@ -10,5 +10,41 @@ package onlineschaken;
  * @author redxice
  */
 public class Chatline {
+    private String userName;
+    private String message;
+    private Game game;
+    private Gamelobby gameLobby;
+    private long time;
+    
+    public Chatline(String userName,String message, Game game)
+    {
+    time = System.currentTimeMillis();;
+    this.userName = userName;
+    this.message = message;
+    this.game = game;
+    }
+    
+    public Chatline(String userName,String message, Gamelobby gameLobby)
+    {
+    this.userName = userName;
+    this.message = message;
+    this.gameLobby = gameLobby;
+    }
+
+    public String getUserName()
+    {
+        return userName;
+    }
+
+    public String getMessage()
+    {
+        return message;
+    }
+    
+    @Override
+    public String toString()            
+    {
+    return System.currentTimeMillis() + this.userName + this.message;
+    }
     
 }
