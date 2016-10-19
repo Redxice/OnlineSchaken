94b296b01b4bebbd739a3b1837d1898420754890
Jeroen van Baast <jeroenvanbaast@gmail.com>
2016-10-19 12:12:36 +0200

    alt shift f

--- OnlineSchaken/src/onlineschaken/Piece.java
+++ OnlineSchaken/src/onlineschaken/Piece.java
@@ -17,78 +17,85 @@ import javafx.stage.Stage;
  *
  * @author redxice
  */
-public abstract class Piece extends StackPane{
+public abstract class Piece extends StackPane
+{
+
     //fields
     String color;
     Player player;
     Section section;
     Image img;
     boolean hasMoved;
-    
+
 //constructor
-    public Piece(String p_color,Player p_player, Section p_section) {
+    public Piece(String p_color, Player p_player, Section p_section)
+    {
         this.color = p_color;
-        this.player = p_player;       
+        this.player = p_player;
         this.section = p_section;
         if (this.section != null)
         {
             this.section.setPiece(this);
         }
-        
+
     }
-    
- 
-   public boolean isValidMove(Section p_section){
-       
-       if (p_section.isOccupied() == true) {
-           if (p_section.getPiece().getColor() != this.color) {
-               return true;
-           }
-           else{
-               return false;
-           }
-       }
-       else{
-           return true;
-       }
-   }
-   
-   
-    public void setSection(Section section) {
+
+    public boolean isValidMove(Section p_section)
+    {
+
+        if (p_section.isOccupied() == true)
+        {
+            if (p_section.getPiece().getColor() != this.color)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        } else
+        {
+            return true;
+        }
+    }
+
+    public void setSection(Section section)
+    {
         this.section = section;
         if (this.section != null)
         {
             section.setPiece(this);
         }
-        
+
     }
-   
-   public  String getColor(){
-       return this.color;
-   }
- //methode
-   public abstract Boolean checkMove(Section p_section);
-    
-   public Boolean move(Section p_section)
-   {
-       for(Piece p : player.pieces)
-       {
-       if(p instanceof King)
-       {
-       if(((King) p).check)
-       {
-       if (!(this instanceof King))
-       {
-           return false;
-       }
-       }
-       }
-       }       
-       try
-       {
-       if(checkMove(p_section))
-       {
-           // kijkt of het gekoze tuk een toren is
+
+    public String getColor()
+    {
+        return this.color;
+    }
+    //methode
+
+    public abstract Boolean checkMove(Section p_section);
+
+    public Boolean move(Section p_section)
+    {
+        for (Piece p : player.pieces)
+        {
+            if (p instanceof King)
+            {
+                if (((King) p).check)
+                {
+                    if (!(this instanceof King))
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+        try
+        {
+            if (checkMove(p_section))
+            {
+                // kijkt of het gekoze tuk een toren is
                 if (section.getPiece() instanceof Rook && section.getPiece().hasMoved == false)
                 {
                     // kijkt of op de gekoze locatie een koning staat
@@ -101,113 +108,111 @@ public abstract class Piece extends StackPane{
                             p_section.getBoard().drawSpecificPieces(p_section, section.getBoard().getSections(2, 0));
                             section.getBoard().getSections()[3][0].setPiece(this);
                             section.getBoard().getSections()[2][0].setPiece(p_section.getPiece());
-                            section.setPiece(null);                     
+                            section.setPiece(null);
                             this.section = section.getBoard().getSections(3, 0);
-                            p_section.getPiece().section = section.getBoard().getSections()[2][0];                            
-                            p_section.setPiece(null);  
+                            p_section.getPiece().section = section.getBoard().getSections()[2][0];
+                            p_section.setPiece(null);
                             return true;
-                        }
-                        else if (section.getID().getX() == 0 && section.getID().getY() == 7)
+                        } else if (section.getID().getX() == 0 && section.getID().getY() == 7)
                         {
                             section.getBoard().drawSpecificPieces(section, section.getBoard().getSections(3, 7));
                             p_section.getBoard().drawSpecificPieces(p_section, section.getBoard().getSections(2, 7));
                             section.getBoard().getSections()[3][7].setPiece(this);
                             section.getBoard().getSections()[2][7].setPiece(p_section.getPiece());
-                            section.setPiece(null);                     
+                            section.setPiece(null);
                             this.section = section.getBoard().getSections(3, 7);
-                            p_section.getPiece().section = section.getBoard().getSections()[2][7];                            
-                            p_section.setPiece(null);  
+                            p_section.getPiece().section = section.getBoard().getSections()[2][7];
+                            p_section.setPiece(null);
                             return true;
-                        }
-                        else if (section.getID().getX() == 7 && section.getID().getY() == 7)
+                        } else if (section.getID().getX() == 7 && section.getID().getY() == 7)
                         {
                             section.getBoard().drawSpecificPieces(section, section.getBoard().getSections(5, 7));
                             p_section.getBoard().drawSpecificPieces(p_section, section.getBoard().getSections(6, 7));
                             section.getBoard().getSections()[5][7].setPiece(this);
                             section.getBoard().getSections()[6][7].setPiece(p_section.getPiece());
-                            section.setPiece(null);                     
+                            section.setPiece(null);
                             this.section = section.getBoard().getSections(5, 7);
-                            p_section.getPiece().section = section.getBoard().getSections()[6][7];                            
-                            p_section.setPiece(null);  
+                            p_section.getPiece().section = section.getBoard().getSections()[6][7];
+                            p_section.setPiece(null);
                             return true;
-                        }
-                        else if (section.getID().getX() == 7 && section.getID().getY() == 0)
+                        } else if (section.getID().getX() == 7 && section.getID().getY() == 0)
                         {
                             section.getBoard().drawSpecificPieces(section, section.getBoard().getSections(5, 0));
                             p_section.getBoard().drawSpecificPieces(p_section, section.getBoard().getSections(6, 0));
                             section.getBoard().getSections()[5][0].setPiece(this);
                             section.getBoard().getSections()[6][0].setPiece(p_section.getPiece());
-                            section.setPiece(null);                     
+                            section.setPiece(null);
                             this.section = section.getBoard().getSections(5, 0);
-                            p_section.getPiece().section = section.getBoard().getSections()[6][0];                            
-                            p_section.setPiece(null);  
+                            p_section.getPiece().section = section.getBoard().getSections()[6][0];
+                            p_section.setPiece(null);
                             return true;
                         }
                     }
                 }
-           section.getBoard().drawSpecificPieces(section, p_section);
-           section.setPiece(null);
-           section.id.x = p_section.id.x;
-           section.id.y = p_section.id.y;
-           section.getBoard().getSections()[section.id.x][section.id.y].setPiece(this);
-           this.section = section.getBoard().getSections()[section.id.x][section.id.y];
-           if (this instanceof Pawn)
-           {
-               Pawn pawn = (Pawn)this;
-               if (pawn.Promotion(p_section))
-               {
-                   Popup menu = pawn.menu();
-                   double x =p_section.getBoard().getWIDTH()*100;
-                   double y=p_section.getBoard().getHEIGHT()*50;
-                   menu.requestFocus();
-                   menu.show(p_section,x,y);
-                  
-               }
-           }
-           hasMoved = true;
-           return true;
-       }
-       return false;
-       }
-       catch(NullPointerException e)
-       {
-           //Logger.getLogger(e.getMessage());
-           System.out.println(e.getMessage());
-           System.out.println(section.getPiece());
-           System.out.println(section.getBoard());
-           System.out.println(p_section.getPiece());
-       }
-           return false;
-   }
+                section.getBoard().drawSpecificPieces(section, p_section);
+                section.setPiece(null);
+                section.id.x = p_section.id.x;
+                section.id.y = p_section.id.y;
+                section.getBoard().getSections()[section.id.x][section.id.y].setPiece(this);
+                this.section = section.getBoard().getSections()[section.id.x][section.id.y];
+                if (this instanceof Pawn)
+                {
+                    Pawn pawn = (Pawn) this;
+                    if (pawn.Promotion(p_section))
+                    {
+                        Popup menu = pawn.menu();
+                        double x = p_section.getBoard().getWIDTH() * 100;
+                        double y = p_section.getBoard().getHEIGHT() * 50;
+                        menu.requestFocus();
+                        menu.show(p_section, x, y);
 
-   public void moveWithoutCheck(Section p_section)
-   {
-       try
-       {
-           section.getBoard().drawSpecificPieces(section, p_section);
-           section.setPiece(null);
-           section.id.x = p_section.id.x;
-           section.id.y = p_section.id.y;
-           section.getBoard().getSections()[section.id.x][section.id.y].setPiece(this);
-           this.section = section.getBoard().getSections()[section.id.x][section.id.y];
-       }
-       catch(NullPointerException e)
-       {
-           //Logger.getLogger(e.getMessage());
-           System.out.println(e.getMessage());
-           System.out.println(section.getPiece());
-           System.out.println(section.getBoard());
-           System.out.println(p_section.getPiece());
-       }
-           hasMoved = true;      
-   }
-    
-   public Section getSection(){
-       return this.section;
-   }
-   //geeft een lijst met opties
-   public List<Section> moveOption(Section p_curSection){
-       List<Section> sections =null;
-      return sections;
-   }
+                    }
+                }
+                hasMoved = true;
+                return true;
+            }
+            return false;
+        } catch (NullPointerException e)
+        {
+            //Logger.getLogger(e.getMessage());
+            System.out.println(e.getMessage());
+            System.out.println(section.getPiece());
+            System.out.println(section.getBoard());
+            System.out.println(p_section.getPiece());
+        }
+        return false;
+    }
+
+    public void moveWithoutCheck(Section p_section)
+    {
+        try
+        {
+            section.getBoard().drawSpecificPieces(section, p_section);
+            section.setPiece(null);
+            section.id.x = p_section.id.x;
+            section.id.y = p_section.id.y;
+            section.getBoard().getSections()[section.id.x][section.id.y].setPiece(this);
+            this.section = section.getBoard().getSections()[section.id.x][section.id.y];
+        } catch (NullPointerException e)
+        {
+            //Logger.getLogger(e.getMessage());
+            System.out.println(e.getMessage());
+            System.out.println(section.getPiece());
+            System.out.println(section.getBoard());
+            System.out.println(p_section.getPiece());
+        }
+        hasMoved = true;
+    }
+
+    public Section getSection()
+    {
+        return this.section;
+    }
+    //geeft een lijst met opties
+
+    public List<Section> moveOption(Section p_curSection)
+    {
+        List<Section> sections = null;
+        return sections;
+    }
 }
