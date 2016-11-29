f84199d1e955c94b3e25d7ec9f8b04e932632b06
Redxice <melvinnboeters@outlook.com>
2016-11-29 14:36:13 +0100

    RegisterController in progress

--- OnlineSchaken/src/gui/LoginController.java
+++ OnlineSchaken/src/gui/LoginController.java
@@ -5,9 +5,20 @@
  */
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,10 +27,30 @@ import javafx.fxml.Initializable;
  */
 public class LoginController implements Initializable
 {
-
+ private static final Logger LOGGER = Logger.getLogger( LoginController.class.getName() );
     /**
      * Initializes the controller class.
      */
+    @FXML
+    public Button BtnRegister;
+    
+    @FXML
+    private void HandleRegisterBTN(ActionEvent event)
+    {
+        try
+        {   Stage LoginStage = (Stage) BtnRegister.getScene().getWindow();
+            LoginStage.close();
+            Stage stage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("Register.fxml"));
+            Scene scene = new Scene(root,Color.TRANSPARENT);
+            stage.setScene(scene);
+            stage.show();
+            
+        } catch (IOException ex)
+        {
+           LOGGER.log(Level.FINE,ex.getMessage());
+        }
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
--- OnlineSchaken/src/gui/RegisterController.java
+++ OnlineSchaken/src/gui/RegisterController.java
@@ -5,9 +5,22 @@
  */
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,10 +29,39 @@ import javafx.fxml.Initializable;
  */
 public class RegisterController implements Initializable
 {
-
+private static final Logger LOGGER = Logger.getLogger( RegisterController.class.getName() );
     /**
      * Initializes the controller class.
      */
+    @FXML
+    private Button Btn_Register;
+    @FXML
+    private TextField TxtField_Username;
+    @FXML
+    private TextField TxtField_Email;
+    @FXML
+    private PasswordField TxtField_Password;
+    @FXML
+    private PasswordField TxtField_RePassword;
+    
+    
+    @FXML
+    private void RegisterAccount(ActionEvent event)
+    {
+        try
+        {  
+            Stage CurrentStage = (Stage) Btn_Register.getScene().getWindow();
+            CurrentStage.close();
+            Stage stage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+            Scene scene = new Scene(root,Color.TRANSPARENT);
+            stage.setScene(scene);
+            stage.show();      
+        } catch (IOException ex)
+        {
+           LOGGER.log(Level.FINE,ex.getMessage());
+        }
+    } 
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
