666a0feed7bd838c6388e932b98b29af7008860d
Jeroen van Baast <jeroenvanbaast@gmail.com>
2016-11-30 12:11:14 +0100

    lobbycontroller

--- OnlineSchaken/src/gui/LobbyController.java
+++ OnlineSchaken/src/gui/LobbyController.java
@@ -5,9 +5,20 @@
  */
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,14 +27,51 @@ import javafx.fxml.Initializable;
  */
 public class LobbyController implements Initializable
 {
-
+   private static final Logger LOGGER = Logger.getLogger( LobbyController.class.getName() );
+   @FXML
+    private Button Btn_Join;
+   @FXML
+    private Button Btn_Profile;
+   @FXML
+    private Button Btn_AddFriend;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
-        // TODO
-    }    
+        
+    }
+    
+    @FXML
+    private void HandleProfileBTN(ActionEvent event)
+    {           
+        try
+        {              
+            Stage CurrentStage = (Stage) Btn_Profile.getScene().getWindow();
+            CurrentStage.close();
+            Stage stage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("profile.fxml"));            
+            Scene scene = new Scene(root,Color.TRANSPARENT);
+            stage.setScene(scene);
+            stage.show();  
+        } catch (IOException ex)
+        {
+           LOGGER.log(Level.FINE,ex.getMessage());
+        }
+    } 
+    
+    @FXML
+    private void joinGame(ActionEvent event)
+    {
+    
+    }
+    
+    @FXML
+    private void addFriend(ActionEvent event)
+    {
+    
+    }
+   
     
 }
--- OnlineSchaken/src/gui/ProfileController.java
+++ OnlineSchaken/src/gui/ProfileController.java
@@ -5,9 +5,20 @@
  */
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,7 +27,11 @@ import javafx.fxml.Initializable;
  */
 public class ProfileController implements Initializable
 {
+    private static final Logger LOGGER = Logger.getLogger( LobbyController.class.getName() );
+    @FXML
+    private Button Btn_Back;
 
+    private int i = 5;
     /**
      * Initializes the controller class.
      */
@@ -25,5 +40,30 @@ public class ProfileController implements Initializable
     {
         // TODO
     }    
+    
+    @FXML
+    private void HandleBack(ActionEvent event)
+    {      
+        System.out.println(i);
+        try
+        {  
+            Stage CurrentStage = (Stage) Btn_Back.getScene().getWindow();
+            CurrentStage.close();
+            Stage stage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("lobby.fxml"));
+            Scene scene = new Scene(root,Color.TRANSPARENT);
+            stage.setScene(scene);
+            stage.show();      
+        } catch (IOException ex)
+        {
+           LOGGER.log(Level.FINE,ex.getMessage());
+        }
+        
+    } 
+
+    public void setI(int i)
+    {
+        this.i = i;
+    }
     
 }
