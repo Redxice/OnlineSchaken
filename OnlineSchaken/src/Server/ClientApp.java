a5cdfe7b72850221db8a5b7a4a04a9ab466ee64b
sander <353331@student.fontys.nl>
2016-12-07 11:18:34 +0100

    code naar klasse clientapp verplaatst

--- OnlineSchaken/src/Server/ClientApp.java
+++ OnlineSchaken/src/Server/ClientApp.java
@@ -5,7 +5,12 @@
  */
 package Server;
 
+import Shared.IrmiServer;
 import java.awt.Point;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import onlineschaken.RMIClient;
 
 /**
@@ -20,8 +25,28 @@ public class ClientApp
      */
     public static void main(String[] args)
     {
-        RMIClient client = new RMIClient();
-        client.sendTurn(new Point(3, 3), new Point(3, 4), "3000");
+        try
+        {
+            Registry registry = LocateRegistry.getRegistry("169.254.183.180", 666);
+            IrmiServer stub;
+            try
+            {
+                stub = (IrmiServer) registry.lookup("setTurn");
+                stub.test();
+                System.out.println("-,-");
+                //stub.doTurn(new Point(3,4), new Point(3,5), "3000");
+                System.out.println("-,0");
+                //stub.test();
+            } catch (NotBoundException e)
+            {
+                System.err.println("Client exception:" + e.toString());
+                e.printStackTrace();
+            }
+        } catch (RemoteException e)
+        {
+            System.err.println("Server exception:" + e.toString());
+            e.printStackTrace();
+        }
     }
     
 }
