6332a49130b66d26ded3af4276b5feb20715d1fb
jeroenvanbaast <jeroenvanbaast@gmail.com>
2016-12-10 13:27:05 +0100

    :shit:  Comments in IrmiServer

--- OnlineSchaken/src/Shared/IrmiServer.java
+++ OnlineSchaken/src/Shared/IrmiServer.java
@@ -21,24 +21,60 @@ import onlineschaken.Player;
  */
 public interface IrmiServer extends Remote
 {
+    /**
+     * Een methoden om een zet door te sturen
+     * @param section1 de sectie waar het stuk op staat dat verplaatst moet worden
+     * @param section2 de sectie waar het stuk naar toe verplaatst moet worden
+     * @param time de tijd die de speler nog overheeft
+     * @throws RemoteException 
+     */
     public void doTurn(Point section1, Point section2, double time) throws RemoteException;
     
     public void test() throws RemoteException;
     
+    /** 
+     * Een methoden om de gamelobbys op te halen
+     * @return list met alle beschikbaren Igamelobbys
+     * @throws RemoteException 
+     */
     public List<IGameLobby> GameLobbys()throws RemoteException;
     
+    /**
+     * Haalt een gamelobby op
+     * @param gamelobbyName
+     * @return de gekozen Igamelobby
+     * @throws RemoteException 
+     */
     public IGameLobby GetIGameLobby(String gamelobbyName)throws RemoteException;
     /**
-     * 
-     * @param naam
-     * @param player1
+     * Er wordt een niewue gamelobby gemaakt
+     * @param naam de naam van de gamelobby
+     * @param player1 de speler die de gamelobby aanmaakt
      * @throws RemoteException 
      */
     public boolean CreateGameLobby(String naam, Player player1)throws RemoteException;
    
+    /**
+     * Een moethoden die een namen van alle gamelobbys terug geeft
+     * @return een lijst met strings van de namen van de Igamelobbys
+     * @throws RemoteException 
+     */
     public ArrayList<String> GetGameLobbys()throws RemoteException;
     
+    /**
+     * Stuurt een bericht naar de chat
+     * @param message De regel die in de chat moet komen
+     * @param naamLobby de naam van de gamelobby waar het bericht is verstuurd
+     * @throws RemoteException 
+     */
     public void SendMessage(Chatline message,String naamLobby)throws RemoteException;
     
+    /**
+     * Zet de speler op ready
+     * @param ready een bool met de waarden of de speler ready is
+     * @param naamLobby de naam van de gamelobby waar de speler zich in bevind
+     * @param userName de naam van de speler die ready heeft geklikt
+     * @throws RemoteException 
+     */
     public void playerReady(boolean ready,String naamLobby, String userName)throws RemoteException;
 }
