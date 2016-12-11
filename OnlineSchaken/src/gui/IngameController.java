ff410464eab7451688ab788ea712ec7d88f49ec7
Redxice <melvinnboeters@outlook.com>
2016-12-11 17:17:47 +0100

    board

--- OnlineSchaken/src/gui/IngameController.java
+++ OnlineSchaken/src/gui/IngameController.java
@@ -13,11 +13,14 @@ import java.net.URL;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Group;
 import javafx.scene.SubScene;
-
+import onlineschaken.Chatline;
 
 import onlineschaken.Game;
 import onlineschaken.Player;
@@ -29,22 +32,26 @@ import onlineschaken.Player;
  */
 public class IngameController extends UnicastRemoteObject implements Initializable, IinGameController
 {
+
     private ClientApp client;
     private IrmiClient Iclient;
-    @FXML 
+    @FXML
     private SubScene GameBoard;
     private Game game;
     private String player1;
     private String player2;
-    
-    public IngameController()throws RemoteException
+
+    public IngameController() throws RemoteException
     {
-        
+
     }
-   /**
-    * moet nog verder worden uitgewerkt. De players moeten worden geadd in de game.
-    */
-    public void DrawBoard() throws RemoteException{
+
+    /**
+     * moet nog verder worden uitgewerkt. De players moeten worden geadd in de
+     * game.
+     */
+    public void DrawBoard() throws RemoteException
+    {
         Player p1 = new Player("White", "ww", 0);
         Player p2 = new Player("Black", "ww", 0);
         Group root = new Group();
@@ -58,70 +65,92 @@ public class IngameController extends UnicastRemoteObject implements Initializab
         game.getBoard().createContent2();
         root.getChildren().add(game.getBoard().getRoot());
     }
-     /**
+
+    /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
-        
-  
-    }    
+
+    }
 
     @Override
-    public void setIClient(IrmiClient iClient)throws RemoteException
+    public void setIClient(IrmiClient iClient) throws RemoteException
     {
-       this.Iclient = iClient;
+        this.Iclient = iClient;
     }
-    
+
     @Override
-    public void setClient(ClientApp client)throws RemoteException
+    public void setClient(ClientApp client) throws RemoteException
     {
-       this.client = client;
+        this.client = client;
     }
-    
+
     @Override
-    public IrmiClient getClient()throws RemoteException
+    public IrmiClient getClient() throws RemoteException
     {
-       return (IrmiClient)client;
+        return (IrmiClient) client;
     }
-    
+
     @Override
-    public IrmiClient getIClient()throws RemoteException
+    public IrmiClient getIClient() throws RemoteException
     {
         return Iclient;
     }
-    
+
     @Override
-    public void move(Point section1, Point section2, double time)throws RemoteException
+    public void move(Point section1, Point section2, double time) throws RemoteException
     {
         System.out.println("Start move methode");
         int xValue = (int) section1.getX();
         int yValue = (int) section1.getY();
         System.out.println(game.getBoard().getSections(xValue, yValue).getPiece().toString());
-        System.out.println(game.getBoard().getSections((int)section2.getX(), (int)section2.getY()).getID());
-        if(game.getBoard().getSections(xValue, yValue).getPiece().move(game.getBoard().getSections((int)section2.getX(), (int)section2.getY())) == false)
+        System.out.println(game.getBoard().getSections((int) section2.getX(), (int) section2.getY()).getID());
+        new Thread(new Runnable()
         {
-            System.out.println("Hij mag daar niet heen bewegen/er gaat iets fout");
-        }
+            @Override
+            public void run()
+            {
+                Platform.runLater(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+
+                        if (game.getBoard().getSections(xValue, yValue).getPiece().move(game.getBoard().getSections((int) section2.getX(), (int) section2.getY())) == false)
+                        {
+                            System.out.println("Hij mag daar niet heen bewegen/er gaat iets fout");
+                        }
+
+                    }
+                });
+            }
+        }).start();
+
         System.out.println("gelukt");
     }
-    
+
     @Override
-    public String getPlayer1() throws RemoteException{
+    public String getPlayer1() throws RemoteException
+    {
         return player1;
     }
-    
-    public void setPlayer1(String player1) {
+
+    public void setPlayer1(String player1)
+    {
         this.player1 = player1;
     }
+
     @Override
-    public String getPlayer2() throws RemoteException{
+    public String getPlayer2() throws RemoteException
+    {
         return player2;
     }
 
-    public void setPlayer2(String player2) {
+    public void setPlayer2(String player2)
+    {
         this.player2 = player2;
     }
-    
+
 }
