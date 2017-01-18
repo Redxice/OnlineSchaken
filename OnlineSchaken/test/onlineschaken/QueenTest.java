2fdd464456d255738106166ee01681b0aedc3b10
sander <sandervanelderen@gmail.com>
2017-01-18 14:31:07 +0100

    generate testen

--- OnlineSchaken/test/onlineschaken/QueenTest.java
+++ OnlineSchaken/test/onlineschaken/QueenTest.java
@@ -5,8 +5,6 @@
  */
 package onlineschaken;
 
-import gui.OnlineSchaken;
-import javafx.scene.image.Image;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -18,72 +16,47 @@ import static org.junit.Assert.*;
  *
  * @author Sander
  */
-public class KnightTest
+public class QueenTest
 {
-
-    private OnlineSchaken onlineSchaken;
-    private Game game;
-    private Player p1;
-    private Player p2;
-    private Board board;
-
-    public KnightTest()
+    
+    public QueenTest()
     {
-        onlineSchaken = new OnlineSchaken();
-        p1 = new Player("p1", "ww", 0);
-        p2 = new Player("p2", "ww", 0);
-        game = new Game(p1, p2);
-        game.getBoard().createContent();
     }
-
+    
     @BeforeClass
     public static void setUpClass()
     {
     }
-
+    
     @AfterClass
     public static void tearDownClass()
     {
     }
-
+    
     @Before
     public void setUp()
     {
     }
-
+    
     @After
     public void tearDown()
     {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    @Test
-    public void TestBlackKnight()
-    {
-        Knight k = new Knight("Black", p1, game.getBoard().getSections(3, 5));
-    }
-
-    @Test
-    public void TestWiteKnight()
-    {
-        Knight k = new Knight("White", p1, game.getBoard().getSections(3, 5));
-    }
-
     /**
-     * Test of checkMove method, of class Knight.
+     * Test of checkMove method, of class Queen.
      */
     @Test
     public void testCheckMove()
     {
         System.out.println("checkMove");
         Section p_section = null;
-        Knight instance = null;
+        Queen instance = null;
         Boolean expResult = null;
         Boolean result = instance.checkMove(p_section);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
+    
 }
